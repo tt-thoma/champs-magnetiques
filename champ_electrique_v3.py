7992694cec1fc1bfffcf7f6cd29cc0dc663ab90d
= <=>
2024-01-17 19:07:11 +0100

    Cleanup + Backup
    Co-authored-by: coulontom808@gmail.com <coulontom808@gmail.com>

--- champ_electrique_v3.py
+++ champ_electrique_v3.py
@@ -3,25 +3,23 @@ import numpy as np
 from math import sqrt
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
-import time
-import random 
+import random
 
 
-#c_coulomb = 8.854 * (10**-12)
+# c_coulomb = 8.854 * (10**-12)
 c_coulomb = 8.854 * (10**-12)
 charge_unit = 10 * 1.602 * 10**-19
 unit_time = 0.001
-
 
 
 class Particle:
     def __init__(
         self, x: int, y: int, charge: float, vx: float = 0.0, vy: float = 0.0
     ) -> None:
-        self.x: float = x 
-        self.y: float = y 
-        self.vx: float = vx 
-        self.vy: float = vy 
+        self.x: float = x
+        self.y: float = y
+        self.vx: float = vx
+        self.vy: float = vy
 
         self.charge: float = charge
 
@@ -31,9 +29,8 @@ class Particle:
             self.mass: float = 1.673 * (10**-27)
         else:
             self.mass: float = 1.675 * (10**-27)
-        #self.mass *= abs(charge)
-        
-    def calc(self, world, size):
+
+    def calc(self, world, size: int) -> None:
         points = np.arange(size**2)
         x = points % size
         y = points // size
@@ -43,23 +40,23 @@ class Particle:
         vec_y = np.where(mask, y - self.y, 0).astype(float)
 
         vec_norm = np.sqrt(vec_x**2 + vec_y**2) + 1e-9
-        force = np.where(mask, abs((c_coulomb * self.charge) / vec_norm**2), 0)
+
+        force = abs(np.where(mask, abs((c_coulomb * self.charge) / vec_norm**2), 0))
 
         uni_mult = force / vec_norm
-        vec_x *= np.sign(self.charge) * uni_mult
-        vec_y *= np.sign(self.charge) * uni_mult
-       
+        vec_x *= uni_mult * np.sign(self.charge)
+        vec_y *= uni_mult * np.sign(self.charge)
+
         world[:, 0] += force
         world[:, 1] += vec_x
         world[:, 2] += vec_y
-               
+
     def calc_next(self, world, size, dt):
         id = int(self.y) * size + int(self.x)
-        Fx = world[id, 1] *self.charge
-        Fy = world[id, 2] *self.charge
-        accel_x = Fx/ self.mass
-        accel_y = Fy/ self.mass
-
+        Fx = world[id, 1] * self.charge
+        Fy = world[id, 2] * self.charge
+        accel_x = Fx / self.mass
+        accel_y = Fy / self.mass
         k1_vx = dt * accel_x
         k1_vy = dt * accel_y
         k1_x = dt * self.vx
@@ -80,53 +77,51 @@ class Particle:
         k4_x = dt * (self.vx + k3_vx)
         k4_y = dt * (self.vy + k3_vy)
 
-        new_vx = self.vx + (k1_vx + 2*k2_vx + 2*k3_vx + k4_vx) / 6
-        new_vy = self.vy + (k1_vy + 2*k2_vy + 2*k3_vy + k4_vy) / 6
-        new_x = self.x + (k1_x + 2*k2_x + 2*k3_x + k4_x) / 6
-        new_y = self.y + (k1_y + 2*k2_y + 2*k3_y + k4_y) / 6
+        new_vx = self.vx + (k1_vx + 2 * k2_vx + 2 * k3_vx + k4_vx) / 6
+        new_vy = self.vy + (k1_vy + 2 * k2_vy + 2 * k3_vy + k4_vy) / 6
+        new_x = self.x + (k1_x + 2 * k2_x + 2 * k3_x + k4_x) / 6
+        new_y = self.y + (k1_y + 2 * k2_y + 2 * k3_y + k4_y) / 6
 
-        # Vérifiez si la particule est sur le point de sortir des limites
-        if new_x < 0 or new_x > size:
-            new_vx = -new_vx*0.5  # Inversez la vitesse en x
+        if new_x <= 0 or new_x >= size:
+            new_vx = -new_vx * 0.5
         else:
-            self.x = new_x  # Mettez à jour la position en x
+            self.x = new_x
 
-        if new_y < 0 or new_y > size:
-            new_vy = -new_vy*0.5  # Inversez la vitesse en y
+        if new_y <= 0 or new_y >= size:
+            new_vy = -new_vy * 0.5
         else:
-            self.y = new_y  # Mettez à jour la position en y
+            self.y = new_y
 
-        self.vx = new_vx  # Mettez à jour la vitesse en x
-        self.vy = new_vy  # Mettez à jour la vitesse en y
+        self.vx = new_vx
+        self.vy = new_vy
 
     def check_collision(self, other):
-        return sqrt((self.x - other.x)**2 + (self.y - other.y)**2) <= np.sqrt(2)
+        return sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2) <= sqrt(2)
 
 
 class World:
     def __init__(self, size: int, dt: float) -> None:
         self.size: int = size
-        self.dt :float = dt
+        self.dt: float = dt
         # Force, vecX, vecY
         self.world = np.zeros((size**2, 3))
         self.particles: list[Particle] = []
-    
+
     def add_part(self, part: Particle) -> None:
         self.particles.append(part)
-    
+
     def calc(self):
         for part in self.particles:
             part.calc(self.world, self.size)
         for i in range(len(self.particles)):
-            for j in range(i+1, len(self.particles)):
+            for j in range(i + 1, len(self.particles)):
                 if self.particles[i].check_collision(self.particles[j]):
-                    
                     self.particles[i].vx = 0
                     self.particles[i].vy = 0
                     self.particles[j].vx = 0
                     self.particles[j].vy = 0
             self.particles[i].calc_next(self.world, self.size, self.dt)
-    
+
     def get_pos(self):
         img = np.zeros((self.size, self.size))
         for part in self.particles:
@@ -136,29 +131,54 @@ class World:
             if x < 0 or y < 0:
                 continue
 
-            print(f"x: {x}, y: {y}")
             img[-y, x] = 1
-
         return img
 
-    
     def animate(self, i):
         self.calc()
         self.img.set_array(self.get_pos())
         return [self.img]
-    
-    def create_animation(self):
-        self.fig, self.ax = plt.subplots()
-        self.ax.axis('on')
-        self.img = self.ax.imshow(self.get_pos(), animated=True)
-        ani = FuncAnimation(self.fig, self.animate, frames=10000, interval=1, blit=True)
+
+    def show_field(self):
+        X, Y = np.meshgrid(np.arange(self.size), np.arange(self.size))
+        fig, ax = plt.subplots()
+        magnitude = np.sqrt(self.world[:, 1] ** 2 + self.world[:, 2] ** 2)
+        ux = self.world[:, 1] / magnitude
+        uy = self.world[:, 2] / magnitude
+
+        Q = ax.quiver(
+            X,
+            Y,
+            ux,
+            uy,
+            scale=None,
+        )
+        ax.quiverkey(
+            Q, X=0.3, Y=1.1, U=10, label="Quiver key, length = 10", labelpos="E"
+        )
         plt.show()
 
-world = World(100,0.01)
-for i in range(100):
-    p = Particle(random.randint(1,98),random.randint(1,98),random.randint(-10,10)*50000*charge_unit)
-    world.add_part((p))
-dt = 0.01   
+    def create_animation(self):
+        fig, ax = plt.subplots()
+        ax.axis("on")
+        self.img = ax.imshow(self.get_pos(), animated=True)
+        ani = FuncAnimation(fig, self.animate, frames=1, interval=1, blit=True)
+        plt.show()
 
-print(f"condition initial : 2 particules 60,40,-100000*charge_unit et 60,60,100000*charge_unit dans un monde 100,0.01  ")
-world.create_animation()
+
+dt = 0.01
+world = World(100, dt)
+"""
+for i in range(2):
+    p = Particle(
+        random.randint(1, 98),
+        random.randint(1, 98),
+        random.randint(-10, 10) * 50000 * charge_unit,
+    )
+    world.add_part((p))
+"""
+world.add_part(Particle(20, 20, 50000 * charge_unit))
+world.add_part(Particle(30, 80, 50000 * charge_unit))
+world.calc()
+world.show_field()
+# world.create_animation()
