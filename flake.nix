e4ee71c90ef38d8b43d643facd4a300d633b883d
Virinas-code <alexcode228@gmail.com>
2025-12-14 14:05:34 +0100

    Flake development environment

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "champs-magnetiques";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              (python313.withPackages (
+                python-pkgs: with python-pkgs; [
+                  numpy
+                  matplotlib
+                  scipy
+                  numba
+                  pytest
+                ]
+              ))
+            ];
+          };
+        }
+      );
+    };
+}
